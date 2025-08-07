3715f755f3bdd0d8de628459cf15d16fcb952b68
K Mani Chandy <kmchandy@gmail.com>
2025-08-07 14:21:27 -0700

    bits

--- dsl/examples/numpy_scaling.py
+++ dsl/examples/numpy_scaling.py
@@ -1,25 +1,34 @@
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 from dsl.core import Network
 from dsl.block_lib.stream_generators import generate
 from dsl.block_lib.stream_transformers import WrapFunction
 from dsl.block_lib.stream_recorders import RecordToList
+import numpy as np
 
-scaler = MinMaxScaler()
-scaler.fit([[1], [2], [3]])
 
-def scale(x): return scaler.transform([[x]])[0][0]
+# NumPy min-max normalization
+def minmax_scale(list_of_numbers):
+    arr = np.array(list_of_numbers)
+    min_val = arr.min()
+    max_val = arr.max()
+    if min_val == max_val:
+        return [0.0 for _ in arr]  # avoid divide by zero
+    return ((arr - min_val) / (max_val - min_val)).tolist()
+
 
 results = []
 
 net = Network(
     blocks={
-        "gen": generate([1, 2, 3], key="x"),
-        "scale": WrapFunction(func=scale, input_key="x", output_key="scaled"),
-        "rec": RecordToList(results)
+        "gen": generate([
+            [1, 2, 3],
+            [10, 20, 30],
+            [5, 5, 5],  # test edge case: constant values
+        ], key="x"),
+        "scale": WrapFunction(func=minmax_scale, input_key="x", output_key="scaled"),
+        "rec": RecordToList(results),
     },
     connections=[("gen", "out", "scale", "in"), ("scale", "out", "rec", "in")]
 )
 
 net.compile_and_run()
-print(results)  # [{'x': 1, 'scaled': 0.0}, {'x': 2, 'scaled': 0.5}, {'x': 3, 'scaled': 1.0}]
+print(results)
