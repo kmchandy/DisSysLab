36b2d9dd371160dec05a9de9809dc2148206d191
K Mani Chandy <kmchandy@gmail.com>
2025-08-07 14:21:43 -0700

    bits

--- dsl/examples/sentiment_analysis.py
+++ dsl/examples/sentiment_analysis.py
@@ -1,14 +1,19 @@
 from dsl.core import Network
 from dsl.block_lib.stream_generators import generate
-from dsl.block_lib.stream_transformers import SentimentClassifierWithGPT
+from dsl.block_lib.stream_transformers import GPT_Prompt
 from dsl.block_lib.stream_recorders import RecordToList
+from dsl.block_lib.prompt_templates import sentiment_prompt, as_list
 
 results = []
 
 net = Network(
     blocks={
         "gen": generate(["I love pizza", "I hate waiting"], key="text"),
-        "clf": SentimentClassifierWithGPT(input_key="text"),
+        "clf": GPT_Prompt(
+            messages=sentiment_prompt,
+            input_key="text",
+            output_key="sentiment"
+        ),
         "rec": RecordToList(results),
     },
     connections=[("gen", "out", "clf", "in"), ("clf", "out", "rec", "in")]
