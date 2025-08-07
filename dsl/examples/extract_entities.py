318d552f758fed404ab142320155d7dc2aafb690
K Mani Chandy <kmchandy@gmail.com>
2025-08-07 14:21:02 -0700

    bits

--- dsl/examples/extract_entities.py
+++ dsl/examples/extract_entities.py
@@ -1,18 +1,35 @@
 from dsl.core import Network
 from dsl.block_lib.stream_generators import generate
-from dsl.block_lib.stream_transformers import ExtractEntitiesWithGPT
+from dsl.block_lib.stream_transformers import GPT_Prompt
 from dsl.block_lib.stream_recorders import RecordToList
+from dsl.block_lib.prompt_templates import entity_extraction_prompt, as_list
 
-results = []
 
-net = Network(
-    blocks={
-        "gen": generate(["Barack Obama was president"], key="text"),
-        "xf": ExtractEntitiesWithGPT(input_key="text"),
-        "rec": RecordToList(results),
-    },
-    connections=[("gen", "out", "xf", "in"), ("xf", "out", "rec", "in")],
-)
+def extract_entities():
+    results = []
 
-net.compile_and_run()
-print(results)  # e.g., [["Barack Obama"]]
+    net = Network(
+        blocks={
+            "gen": generate(
+                [
+                    "Barack Obama was president",
+                    "Seward bought Alaska from Russia",
+                    "Jaguars are cats"
+                ], key="text"),
+            "xf": GPT_Prompt(
+                messages=entity_extraction_prompt,
+                postprocess_fn=as_list,
+                input_key="text",
+                output_key="entities"
+            ),
+            "rec": RecordToList(results),
+        },
+        connections=[("gen", "out", "xf", "in"), ("xf", "out", "rec", "in")],
+    )
+
+    net.compile_and_run()
+    print(results)
+
+
+if __name__ == "__main__":
+    extract_entities()
