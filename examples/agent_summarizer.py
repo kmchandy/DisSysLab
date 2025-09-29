a06462983bfeae1af5fdf7dd9a49df7304e8198e
K Mani Chandy <kmchandy@gmail.com>
2025-09-29 09:55:13 -0700

    added agent examples

---
+++ examples/agent_summarizer.py
@@ -0,0 +1,51 @@
+from dsl import Graph
+from dsl.extensions.agent_openai import AgentOpenAI
+
+list_of_text = [
+
+    ("A play is a form of theatre that primarily consists of"
+     "script between speakers and is intended for acting rather"
+     "than mere reading. The writer and author of a play is"
+     "known as a playwright. Plays are staged at various levels,"
+     "ranging from London's West End and New York City's "
+     "Broadway – the highest echelons of commercial theatre in"
+     "the English-speaking world – to regional theatre, community"
+     "theatre, and academic productions at universities and schools."
+     )
+]
+
+
+def from_list_of_text():
+    for data_item in list_of_text:
+        yield {"text": data_item}
+
+
+results = []
+def to_results(v): results.append(v)
+
+
+add_key = "summary"
+
+
+def agent_op(v):
+    v[add_key] = agent.fn(v["text"])
+    return v
+
+
+system_prompt = "Summarize the text in a single line."
+agent = AgentOpenAI(system_prompt=system_prompt)
+
+
+g = Graph(
+    edges=[("src", "trn"), ("trn", "snk")],
+    nodes=[("src", from_list_of_text),
+           ("trn", agent_op), ("snk", to_results)]
+)
+g.compile_and_run()
+
+if __name__ == "__main__":
+    for result in results:
+        for key, value in result.items():
+            print(key)
+            print(value)
+        print("")
