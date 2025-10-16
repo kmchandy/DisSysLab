f5135911465811504aa96ec6c9efd3d564096259
K Mani Chandy <kmchandy@gmail.com>
2025-10-16 09:46:24 -0700

    changed simple_anomaly to be a network

--- modules/ch04_numeric/simple_anomaly.py
+++ modules/ch04_numeric/simple_anomaly.py
@@ -6,6 +6,8 @@ from dsl.connectors.replay_csv_in import ReplayCSV_In
 from .rolling_stats_anom_forecast import rolling_stats_anom_forecast
 from .temp_live_sink import temp_live_sink
 
+# -------------------------------------------------------------------------
+# Source: Generate historical daily max temperature from Open-Meteo
 CSV_PATH = str(Path(__file__).resolve().parent / "open-meteo_clean.csv")
 
 
@@ -19,6 +21,9 @@ def transform_row(row):
 
 replay = ReplayCSV_In(path=CSV_PATH, transform=transform_row, period_s=0.25)
 
+
+# -------------------------------------------------------------------------
+# Transform: Rolling statistics for anomaly detection and forecasting
 xf = rolling_stats_anom_forecast(
     window=20,
     k_anom=2.0,      # anomaly threshold
@@ -28,13 +33,11 @@ xf = rolling_stats_anom_forecast(
     prefix="w20"
 )
 
+# -------------------------------------------------------------------------
+# Sink: Use temp_live_sink to display results
 
-def temperature_source():
-    for msg in replay.run():
-        if msg is None:
-            continue
-        yield xf(msg)
+# -------------------------------------------------------------------------
+# Network: Connect functions
 
-
-g = network([(temperature_source, temp_live_sink)])
+g = network([(replay.run, xf), (xf, temp_live_sink)])
 g.run_network()
